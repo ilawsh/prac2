58313cc332314a12e9a3edf7666d461371b8c219
ilawsh <146442402+ilawsh@users.noreply.github.com>
2024-02-27 21:00:23 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let MainSec = document.querySelector('.Main-1')
+let MenuBTN = document.querySelector('.btnmenu')
+let Menu = document.querySelector('.Navbar-5')
+let X = document.querySelector('.x')
+let MainIMG = document.querySelector('.Main-2')
+
+
+MenuBTN.addEventListener('click' ,  () => {
+    Menu.style.right = 0 + '%'
+    Menu.style.opacity = 1
+    MainSec.style.filter = 'blur(' + 2 +'px)'
+    MainIMG.style.filter = 'blur(' + 2 +'px)'
+    X.style.display = 'block'
+    MenuBTN.style.opacity = 0;
+})
+
+
+X.addEventListener('click' , () => {
+    Menu.style.right = -100 + '%'
+    Menu.style.opacity = 0
+    MainSec.style.filter = 'blur(' + 0 +'px)'
+    MainIMG.style.filter = 'blur(' + 0 +'px)'
+    X.style.display = 'none'
+    MenuBTN.style.display = 'block'
+    MenuBTN.style.opacity = 1;
+})
+
+MainSec.addEventListener('click' , () => {
+    Menu.style.right = -100 + '%'
+    MainSec.style.filter = 'blur(' + 0 +'px)'
+    X.style.display = 'none'
+    MenuBTN.style.display = 'block'
+    MenuBTN.style.opacity = 1;
+})
+
+Menu.addEventListener('click' , () => {
+    Menu.style.right = -100 + '%'
+    MainSec.style.filter = 'blur(' + 0 +'px)'
+    X.style.display = 'none'
+    MenuBTN.style.display = 'block'
+    MenuBTN.style.opacity = 1;
+})
